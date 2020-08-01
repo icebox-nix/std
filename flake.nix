0953c6d7754742535776668e40f7004574619480
Harry Ying <lexugeyky@outlook.com>
2020-08-01 19:22:07 +0800

    Correct flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,4 @@
 {
   description = "Standard library for icebox";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs }: { nixosModule = (import ./default.nix); };
+  outputs = { ... }: { nixosModule = (import ./default.nix); };
 }
