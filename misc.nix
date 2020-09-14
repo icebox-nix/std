91af3fef69247eb1172286c9848d965a35ec41d0
Harry Ying <lexugeyky@outlook.com>
2020-09-14 20:55:28 +0800

    Add restartServicesOnResume option

---
+++ misc.nix
@@ -0,0 +1,26 @@
+{ config, lib, ... }:
+with lib;
+
+let
+  cfg = config.std.misc;
+  tryRestartCommand = n: ''
+    ${config.systemd.package}/bin/systemctl try-restart ${n}
+  '';
+  serviceResumeCommands = (lists.foldr (a: b: a + b) ""
+    (lists.forEach cfg.restartOnResumeServices (x: tryRestartCommand x)));
+in {
+  options.std.misc = {
+    restartOnResumeServices = mkOption {
+      type = with types; nullOr (listOf str);
+      default = null;
+      description =
+        "Names of the services to restart on resume if they are already running. Appended at the end.";
+    };
+  };
+
+  config = {
+    powerManagement.resumeCommands =
+      optionalString (cfg.restartOnResumeServices != null)
+      (mkAfter serviceResumeCommands);
+  };
+}
